d9d951a0d6b634b50c1df52bc86176d9d981e1c9
Arthur-Agadzhanyan <artur0505gold@gmail.com>
2021-03-23 00:48:34 +0300

    create: добавлена возможность скачивания видео и аудио раздельно

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/download', (req, res) => {
 
     let infoFormats = ''
 
-    async function fuck() {
+    async function download() {
         let info = await ytdl.getInfo(url);
         infoFormats = info.formats.filter(video=>{
             if(video.qualityLabel && video.audioBitrate){
@@ -28,6 +28,50 @@ app.get('/download', (req, res) => {
         }    
     }
     fuck()  
+})
+
+app.get('/download-audio', (req, res) => {
+    let url = req.query.url
+    // res.json({url})
+    res.header('Content-Disposition', 'attachment; filename="audio.webm"');
+
+    async function downloadAudio() {
+        let info = await ytdl.getInfo(url);
+        let audio = ytdl.filterFormats(info.formats, 'audioonly')[0]
+        if(audio){
+            console.log(audio);
+            ytdl(url, {
+                quality: audio.itag,
+                filter: format => format.container === 'webm',
+                format: 'webm'
+            }).pipe(res)
+        }    
+    }
+    downloadAudio()  
+})
+
+app.get('/download-video', (req, res) => {
+    let url = req.query.url
+    // res.json({url})
+    res.header('Content-Disposition', 'attachment; filename="video.mp4"');
+
+    async function downloadVideo() {
+        let info = await ytdl.getInfo(url);
+        let video = ytdl.filterFormats(info.formats, 'videoonly').filter(video=>{
+            if(video.container == "mp4" && video.itag < 140){
+                return video
+            }
+        })[0]
+        if(video){
+            console.log(video);
+            ytdl(url, {
+                quality: video.itag,
+                filter: format => format.container === 'mp4',
+                format: 'mp4'
+            }).pipe(res)
+        }    
+    }
+    downloadVideo()  
 })
 
 app.listen(5000, () => {

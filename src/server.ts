9f8fa79cc08e997cdf5666ef4471b88bf598ea5c
Arthur-Agadzhanyan <artur0505gold@gmail.com>
2021-08-10 21:01:07 +0300

    Heroku

--- src/server.ts
+++ src/server.ts
@@ -6,8 +6,8 @@ const app: express.Application = express()
 
 app.use(cors())
 
-app.get('/',(req,res)=>{
-    res.json({message:'Page Not Found'})
+app.get('/', (req, res) => {
+    res.json({ message: 'Page Not Found' })
 })
 
 app.get('/download', (req, res) => {
@@ -15,21 +15,26 @@ app.get('/download', (req, res) => {
     res.header('Content-Disposition', 'attachment; filename="video.mp4"');
 
     async function download() {
-        let info = await ytdl.getInfo(url);
-        let audioAndVideoFormats = info.formats.filter(video=>{
-            if(video.qualityLabel && video.audioBitrate){
-                return video
+        try {
+            let info = await ytdl.getInfo(url);
+            let audioAndVideoFormats = info.formats.filter(video => {
+                if (video.qualityLabel && video.audioBitrate) {
+                    return video
+                }
+            })
+            if (audioAndVideoFormats) {
+                console.log(audioAndVideoFormats)
+                ytdl(url, {
+                    quality: audioAndVideoFormats[audioAndVideoFormats.length - 1].itag,
+                    filter: format => format.container === 'mp4'
+                }).pipe(res)
             }
-        })
-        if(audioAndVideoFormats){
-            console.log(audioAndVideoFormats)
-            ytdl(url, {
-                quality: audioAndVideoFormats[audioAndVideoFormats.length - 1].itag,
-                filter: format => format.container === 'mp4'
-            }).pipe(res)
-        }    
+        } catch (error) {
+            console.log(error)
+        }
+
     }
-    download()  
+    download()
 })
 
 app.get('/download-audio', (req, res) => {
@@ -37,18 +42,20 @@ app.get('/download-audio', (req, res) => {
     res.header('Content-Disposition', 'attachment; filename="audio.mp3"');
 
     async function downloadAudio() {
-        let info = await ytdl.getInfo(url);
-        // console.log(info)
-        let audio = ytdl.filterFormats(info.formats, 'audioonly')[0]
-        console.log(`Audio \n\n\n`,audio)
-        if(audio){
-            ytdl(url, {
-                quality: audio.itag,
-                filter: format => format.container === 'webm' || format.container === 'mp4'
-            }).pipe(res)
-        }    
+        try {
+            let info = await ytdl.getInfo(url);
+            let audio = ytdl.filterFormats(info.formats, 'audioonly')[0]
+            if (audio) {
+                ytdl(url, {
+                    quality: audio.itag,
+                    filter: format => format.container === 'webm' || format.container === 'mp4'
+                }).pipe(res)
+            }
+        } catch (e) {
+            console.log(e)
+        }
     }
-    downloadAudio()  
+    downloadAudio()
 })
 
 app.get('/download-video', (req, res) => {
@@ -56,22 +63,27 @@ app.get('/download-video', (req, res) => {
     res.header('Content-Disposition', 'attachment; filename="video.mp4"');
 
     async function downloadVideo() {
-        let info = await ytdl.getInfo(url);
-        let video = ytdl.filterFormats(info.formats, 'videoonly').filter(video=>{
-            if(video.container == "mp4" && video.itag < 140){
-                return video
+        try {
+            let info = await ytdl.getInfo(url);
+            let video = ytdl.filterFormats(info.formats, 'videoonly').filter(video => {
+                if (video.container == "mp4" && video.itag < 140) {
+                    return video
+                }
+            })[0]
+            if (video) {
+                ytdl(url, {
+                    quality: video.itag,
+                    filter: format => format.container === 'mp4'
+                }).pipe(res)
             }
-        })[0]
-        if(video){
-            ytdl(url, {
-                quality: video.itag,
-                filter: format => format.container === 'mp4'
-            }).pipe(res)
-        }    
+        } catch (error) {
+            console.log(error)
+        }
+
     }
-    downloadVideo()  
+    downloadVideo()
 })
 
-app.listen(process.env.PORT || 5000 , () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log('Сервер запущен');
 })
